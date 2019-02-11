01de131e1f03e623aa9c10c3877d3654cf34e7ab
LJ <lindajang94@hotmail.com>
2019-02-10 18:15:15 -0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/fi2lms', true);
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  data.forEach(movie => {
+    // Log each movie's title
+    console.log(movie.title);
+  });
+  }
+
+// Send request
+request.send();
