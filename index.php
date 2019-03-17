0060fa9be862829bd7167857d5059aecfd4a2c46
lindajang94 <32085043+lindajang94@users.noreply.github.com>
2019-03-16 17:16:36 -0700

    Add files via upload
    Just merging files from 2 weeks ago

--- index.php
+++ index.php
@@ -5,6 +5,19 @@
 <section class="main-container">
     <div class="main-wrapper">
         <h2>SFUSAT GROUND STATION</h2>
+       
+        <?php
+echo ' 
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<body>
+<img src="https://www.pixalytics.com/wp-content/uploads/2016/01/16899602_ml.jpg" width="280" height="125" title="SAT" alt="SATPIC" class= "center"/>
+</body>
+</html>
+';
+?>
+       
+
     </div>
 </section>
 
